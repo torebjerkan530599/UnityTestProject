426be99a16d069635a654da35f9fe9a29614408b
Tore Bjerkan <torebjerkan@gmail.com>
2025-06-14 12:16:42 +0200

    fix: delete some lines of code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,14 +34,6 @@ public class PlayerController : MonoBehaviour
             //float horizontal = Input.GetAxis("Horizontal"); //*
             float vertical = Input.GetAxis("Vertical"); //only use forward facing direction
 
-            // Combine input with camera's forward and right vectors
-            movementDirection = (cameraForward * vertical); //+ (cameraRight * horizontal);
-            movementDirection.y = 0; // Ensure no vertical movement
-            movementDirection.Normalize();
-
-            // Move the player
-            transform.position += speed * Time.deltaTime * movementDirection;
-            //transform.Translate(speed * Time.deltaTime * movementDirection); //Local space by default, and less control compared to line above
     }
 
 }
